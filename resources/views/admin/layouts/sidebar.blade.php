8d63d650f9274574de7f529cb99576249ff856d0
Malak Milad <malakmilad202@gmail.com>
2025-02-05 21:52:24 +0200

    add dashborad

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,45 @@
+ <aside id="sidebar" class="sidebar">
+     <ul class="sidebar-nav" id="sidebar-nav">
+         <li class="nav-item">
+             <a class="nav-link " href="index.html">
+                 <i class="bi bi-grid"></i>
+                 <span>Dashboard</span>
+             </a>
+         </li>
+         <li class="nav-heading">Pages</li>
+         <li class="nav-item">
+             <a class="nav-link collapsed" data-bs-target="#category" data-bs-toggle="collapse" href="#">
+                 <i class="bi bi-menu-button-wide"></i><span>Category</span><i class="bi bi-chevron-down ms-auto"></i>
+             </a>
+             <ul id="category" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                 <li>
+                     <a href="components-alerts.html">
+                         <i class="bi bi-circle"></i><span>All Categories</span>
+                     </a>
+                 </li>
+                 <li>
+                     <a href="components-accordion.html">
+                         <i class="bi bi-circle"></i><span>Create Category</span>
+                     </a>
+                 </li>
+             </ul>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#Product" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-menu-button-wide"></i><span>Product</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="Product" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="components-alerts.html">
+                        <i class="bi bi-circle"></i><span>All Product</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="components-accordion.html">
+                        <i class="bi bi-circle"></i><span>Create Product</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+     </ul>
+ </aside>
